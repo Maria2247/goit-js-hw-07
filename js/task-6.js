5ad0a48633841c8569b95feaefa769db8f818778
Maria2247 <mashasavchenko@yahoo.com>
2024-04-17 10:40:05 +0300

    task6-3rd try

--- js/task-6.js
+++ js/task-6.js
@@ -13,18 +13,16 @@ const destroyBtn = document.querySelector('[data-destroy]');
 const createBoxes = event => {
   const userInput = Number(inputEl.value);
   if (userInput >= minNum && userInput <= maxNum) {
-    let boxesArray = [];
+    let boxesMarkup = '';
     boxesEl.innerHTML = ' ';
     let size = 30;
     for (let i = minNum; i <= userInput; i++) {
-      const addedBox = document.createElement('div');
-      addedBox.style.width = `${size}px`;
-      addedBox.style.height = `${size}px`;
-      addedBox.style.backgroundColor = getRandomHexColor();
-      boxesArray.push(addedBox);
+      boxesMarkup += `<div style="width: ${size}px; height: ${size}px; background-color:${getRandomHexColor()}"></div>`;
+
       size += 10;
     }
-    const boxesMarkup = boxesArray.map(box => box.outerHTML).join('');
+
+    console.log('boxesMarkup: ', boxesMarkup);
     boxesEl.insertAdjacentHTML('beforeend', boxesMarkup);
   }
   inputEl.value = '';
@@ -37,6 +35,38 @@ function destroyBoxes() {
   inputEl.value = '';
 }
 destroyBtn.addEventListener('click', destroyBoxes);
+
+// Друга спроба
+
+// const createBoxes = event => {
+//   const userInput = Number(inputEl.value);
+//   if (userInput >= minNum && userInput <= maxNum) {
+//     let boxesArray = [];
+//     boxesEl.innerHTML = ' ';
+//     let size = 30;
+//     for (let i = minNum; i <= userInput; i++) {
+//       const addedBox = document.createElement('div');
+//       addedBox.style.width = `${size}px`;
+//       addedBox.style.height = `${size}px`;
+//       addedBox.style.backgroundColor = getRandomHexColor();
+//       boxesArray.push(addedBox);
+//       size += 10;
+//     }
+//     const boxesMarkup = boxesArray.map(box => box.outerHTML).join('');
+//     boxesEl.insertAdjacentHTML('beforeend', boxesMarkup);
+//   }
+//   inputEl.value = '';
+// };
+
+// createBtn.addEventListener('click', createBoxes);
+
+// function destroyBoxes() {
+//   boxesEl.innerHTML = '';
+//   inputEl.value = '';
+// }
+// destroyBtn.addEventListener('click', destroyBoxes);
+
+// Перша спроба
 
 // const amount = () => {
 //   let userInput = Number(inputEl.value);
